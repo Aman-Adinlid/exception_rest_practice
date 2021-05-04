b4c57bd513cc8d54b3daef9fbc4705c8daaa7f2e
aman adinlid <95pinkpanda@gmail.com>
2021-05-05 00:29:13 +0200

    ##Created a config app##

--- src/main/java/se/lexicon/aman/exception_rest/config/AppConfig.java
+++ src/main/java/se/lexicon/aman/exception_rest/config/AppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    @Bean
+    @Bean //bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
