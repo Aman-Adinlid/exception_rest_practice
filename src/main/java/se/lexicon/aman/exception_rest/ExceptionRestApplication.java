17a561893fe65bd0fd1a1e925ad110b0221c96e3
aman adinlid <95pinkpanda@gmail.com>
2021-05-04 17:17:53 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/aman/exception_rest/ExceptionRestApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.aman.exception_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/aman/exception_rest/ExceptionRestApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.aman.exception_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
